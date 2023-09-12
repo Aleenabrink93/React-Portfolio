43c6069b1b2223f05ebdfeb683a0c4c429180a6a
Aleenabrink93 <aleenabrink@gmail.com>
2023-09-12 10:50:15 -0600

    setup nav component

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './nav.css'
 
-const Nav = () => {
+const Nav = (props) => {
+    const {currentTab,setCurrentTab} = props
   return (
-    <div>nav</div>
+    <nav>
+			<ul className="flex-row mobile-view">
+				<li className={currentTab === "about" ? "mx-2 navActive" : "mx-2"}>
+					<span onClick={() => setCurrentTab("about")}>About Me</span>
+				</li>
+				<li className={currentTab === "portfolio" ? "mx-2 navActive" : "mx-2"}>
+					<span onClick={() => setCurrentTab("portfolio")}>Portfolio</span>
+				</li>
+				<li className={currentTab === "contact" ? "mx-2 navActive" : "mx-2"}>
+					<span onClick={() => setCurrentTab("contact")}>Contact</span>
+				</li>
+				<li className={currentTab === "resume" ? "mx-2 navActive" : "mx-2"}>
+					<span onClick={() => setCurrentTab("resume")}>Resume</span>
+				</li>
+			</ul>
+		</nav>
+    
   )
 }
 
