a4824e4f8ee02ab5fa59921b7a986b29e33482ce
Aleenabrink93 <aleenabrink@gmail.com>
2023-09-12 10:47:37 -0600

    removed nav

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,4 @@
 import React,{useState} from 'react'
-// import Nav from './components/nav/nav'
 import Header from './components/header/header'
 import Footer from './components/footer/footer'
 import About from './components/about/about'
