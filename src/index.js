d91c9dc2ba5d6381252f45b2bf37c8bd8de761ac
Aleenabrink93 <aleenabrink@gmail.com>
2023-09-13 10:51:37 -0600

    imported bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import './index.css';
+import 'react-bootstrap';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
