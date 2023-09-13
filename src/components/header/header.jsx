b5d07c6003988caf8a76e451a3fb2b0da01cad13
Aleenabrink93 <aleenabrink@gmail.com>
2023-09-13 09:29:06 -0600

    linked nav bar to header section

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Nav from '../nav/nav'
 import './header.css'
 
-const Header = () => {
+const Header = (props) => {
+    const {currentTab,setCurrentTab} = props
   return (
-    <div>header</div>
+    <header>
+        <div>
+            <h1>Aleena Brinkerhoff</h1>
+        </div>
+        <div>
+            <Nav currentTab={currentTab} setCurrentTab={setCurrentTab}>
+            </Nav>
+        </div>
+    </header>
   )
 }
 
-export default Header
+export default Header;
