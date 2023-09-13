7b692be8a0e3a7416bc71dc379e6c2ed52f58f8f
Aleenabrink93 <aleenabrink@gmail.com>
2023-09-13 10:52:08 -0600

    added resume preview and resume download button

--- src/components/resume/resume.jsx
+++ src/components/resume/resume.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './resume.css'
+import resume from '../../assets/resume.png'
+import Button from 'react-bootstrap/Button';
 
-const resume = () => {
+const Resume = () => {
   return (
-    <div>resume</div>
+    <section>
+    <div>
+        <img src={resume} alt="resume" />
+    </div>
+    <Button variant="outline-warning">
+      <a href = {require('../../assets/CV_AleenaBrinkerhoff.pdf')} download>
+        <h3>Download</h3>
+      </a>
+    </Button>
+    </section>
   )
 }
 
-export default resume
+export default Resume
