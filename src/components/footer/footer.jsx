78f749c990b45e92c6c36c3746f839ef787f6c2a
Aleenabrink93 <aleenabrink@gmail.com>
2023-09-13 09:29:42 -0600

    added hyperlinked icons to footer section

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './footer.css'
+import github from '../../assets/github.png'
+import linkedin from '../../assets/linkedin.png'
 
 const Footer = () => {
   return (
-    <div>footer</div>
+    <div>
+        <a href = 'https://github.com/Aleenabrink93'
+        target = '_blank'
+        rel ='noopener noreferrer'>
+            <img src = {github} alt = 'github' className = 'icon'></img>
+        </a>
+        <a href = 'https://www.linkedin.com/in/aleena-brinkerhoff-49248a151/'
+        target = '_blank'
+        rel ='noopener noreferrer'>
+            <img src = {linkedin} alt = 'linkedin' className = 'icon'></img>
+        </a>
+    </div>
   )
 }
 
