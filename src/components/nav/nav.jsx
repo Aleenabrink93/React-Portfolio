7a18a6f986011ed6ea7719a58629cf3f7055f2f1
Aleenabrink93 <aleenabrink@gmail.com>
2023-09-13 20:15:53 -0600

    Styled nav bar (made it responsive)

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import './nav.css'
 
-const Nav = (props) => {
+const Navbar = (props) => {
     const {currentTab,setCurrentTab} = props
   return (
     <nav>
-			<ul className="flex-row mobile-view">
-				<li className={currentTab === "about" ? "mx-2 navActive" : "mx-2"}>
+			<ul>
+				<li>
 					<span onClick={() => setCurrentTab("about")}>About Me</span>
-				</li>
-				<li className={currentTab === "portfolio" ? "mx-2 navActive" : "mx-2"}>
+                </li>
+				<li className={currentTab === "portfolio" ? "navActive" : "mx-2"}>
 					<span onClick={() => setCurrentTab("portfolio")}>Portfolio</span>
 				</li>
-				<li className={currentTab === "contact" ? "mx-2 navActive" : "mx-2"}>
+				<li className={currentTab === "contact" ? "navActive" : "mx-2"}>
 					<span onClick={() => setCurrentTab("contact")}>Contact</span>
 				</li>
-				<li className={currentTab === "resume" ? "mx-2 navActive" : "mx-2"}>
+				<li className={currentTab === "resume" ? "navActive" : "mx-2"}>
 					<span onClick={() => setCurrentTab("resume")}>Resume</span>
 				</li>
 			</ul>
@@ -24,4 +24,5 @@ const Nav = (props) => {
   )
 }
 
-export default Nav
+
+export default Navbar
